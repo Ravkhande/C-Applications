31cb159c4b680b94ffbc871ed2e09139ee4b5fd5
Pratik Pralhad Ravkhande <62715180+Ravkhande@users.noreply.github.com>
2020-12-21 14:37:10 +0530

    Update and rename assignment22_Q4.c to program9.c

--- Problems on N numbers/program9.c
+++ Problems on N numbers/program9.c
@@ -1,4 +1,12 @@
-
+/*
+Accept N numbers from user and return frequency of 11 form it.
+Input : N : 6
+ Elements : 85 66 3 15 93 88
+Output : 0
+Input : N : 6
+ Elements : 85 11 3 15 11 111
+Output : 2 
+*/
 
 #include <stdio.h>  // for printf and scanf Library function
 #include <stdlib.h> //for malloc Library function
