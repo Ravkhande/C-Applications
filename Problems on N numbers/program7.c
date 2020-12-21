bd72f904266529eb2b804ae04b04bcc5eb6c4a3a
Pratik Pralhad Ravkhande <62715180+Ravkhande@users.noreply.github.com>
2020-12-21 14:34:21 +0530

    Update and rename assignment22_Q2.c to program7.c

--- Problems on N numbers/program7.c
+++ Problems on N numbers/program7.c
@@ -1,4 +1,10 @@
-
+/*
+Accept N numbers from user and return difference between frequency of
+even number and odd numbers.
+Input : N : 7
+ Elements : 85 66 3 80 93 88 90
+Output : 1 (4 -3)
+*/
 
 #include <stdio.h>  // for printf and scanf Library function
 #include <stdlib.h> //for malloc Library function
