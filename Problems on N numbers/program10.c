d304978372c1c47be121adb5a1229306a79972e2
Pratik Pralhad Ravkhande <62715180+Ravkhande@users.noreply.github.com>
2020-12-21 14:38:24 +0530

    Update and rename assignment22_Q5.c to program10.c

--- Problems on N numbers/program10.c
+++ Problems on N numbers/program10.c
@@ -1,4 +1,14 @@
+/*
+Input : N : 6
+ NO: 66
+ Elements : 85 66 3 66 93 88
+Output : 2
+Input : N : 6
+ NO: 12
+ Elements : 85 11 3 15 11 111
+Output : 0 
 
+*/
 #include <stdio.h>  // for printf and scanf Library function
 #include <stdlib.h> //for malloc Library function
 
