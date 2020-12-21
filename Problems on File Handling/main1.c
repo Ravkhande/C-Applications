9077ec3247e53c51c44494be6122b2fb301f24b6
Pratik Pralhad Ravkhande <62715180+Ravkhande@users.noreply.github.com>
2020-12-21 16:00:43 +0530

    Add files via upload

--- Problems on File Handling/main1.c
+++ Problems on File Handling/main1.c
@@ -1,32 +1,54 @@
 
-/* Write application which accept file name from user and open that file in read mode.
+/* Write a program which accepts file name from user and count number of
+capital characters from that file.
+
 Input : Demo.txt
-Output : File opened successfully. 
+Output : Number of capital characters are 23 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <io.h>
 
-int main()
+int CountCapital(char FName[])
 {
-    char name[20] = {'\0'};
-    int fd = 0;
+    char arr[100] = {'\0'};
+    int fd = 0, ret = 0, iCnt = 0, i = 0;
 
-    printf("Enter the file name:\n");
-    scanf("%s", name);
-
-    fd = open(name, O_RDONLY | O_CREAT);
+    fd = open(FName, O_RDONLY);
     if (fd == -1)
     {
-        printf("Unnable to open that file\n");
+        printf("Unnable to read the file\n");
+        return -1;
     }
-    else
+
+    while ((ret = read(fd, arr, sizeof(arr))) != 0)
     {
-        printf("File is sucusefully opened with file descriptor : %d\n", fd);
+        for (i = 0; i < ret; i++)
+        {
+            if ((arr[i] >= 'A') && (arr[i] <= 'Z'))
+            {
+                iCnt++;
+            }
+        }
     }
 
     close(fd);
+    return iCnt;
+}
+
+int main()
+{
+    char FileName[30] = {'\0'};
+    int iRet = 0;
+
+    printf("Enter the file name:\n");
+    scanf("%s", FileName);
+
+    iRet = CountCapital(FileName);
+    printf("Number of Capital characters are : %d\n", iRet);
+
     return 0;
 }
